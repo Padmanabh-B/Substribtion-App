0cca3c919d478d6bfce8dbd3097e2ffe5ce2707e
padmanabh <pro.paddusonu@gmail.com>
2022-12-01 05:56:17 +0530

    destructed user model

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,21 +1,44 @@
-const express = require("express")
+const express = require("express");
+const User = require("../models/user");
+const { hashPassword, comparePassword } = require("../helpers/auth")
 
 exports.register = async (req, res) => {
     try {
         //validation
-        const {name, password} = req.body;
-        if(!name){
+        const { name, email, password } = req.body;
+        if (!name) {
             return res.json({
-                error:'Name is required'
+                error: 'Name is required'
             })
         }
-        if(!password || password.length<6){
+        if (!password || password.length < 6) {
             return res.json({
-                error:"Password is required and must me more than 6"
+                error: "Password is required and must me more than 6"
             })
         }
+
+        
+        //hash password
+        const hashedPassword = await hashPassword(password);
+
+        try {
+            const user = await new User({
+                name, email, password: hashedPassword
+            }).save();
+            console.log(user);
+
+            const { password, ...rest } = user._doc;
+            return res.json()
+
+        } catch (error) {
+            console.log(error);
+        }
+
+
+
+
         res.json({
-            data:"this is register endpoint"
+            data: "this is register endpoint"
         })
 
 
---
+++ server/models/user.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose")
+
+const userSchema = new mongoose.Schema({
+    name:{
+        type:String, 
+        trim:true,
+        required:true,
+    },
+    email:{
+        type:String, 
+        trim:true,
+        unique:true,
+    },
+    password:{
+        type:String,
+        required:true,
+        min:6,
+        max:64,
+    }
+});
+
+module.exports = mongoose.model('User', userSchema);
